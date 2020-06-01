2b3edf1dfc19723d7d41f558f314cb8f85c7af07
renanfelice <felice_owns@hotmail.com>
2020-05-31 22:45:00 -0300

    estilo navbar alterado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,32 +3,26 @@ import './NavBar.css'
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            
-            <a className="navbar-brand" href="/">Navbar</a>
+
+            <a className="navbar-brand" href="/"><img className="home-img" alt='cinemaicon'
+                        src={require('../cinema.png')} /><span className='home-text'>Home</span></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                
+
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search Movie..." aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">GO!</button>
+                </form>
+
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Link</a>
+                        <a className="nav-link about" href="/">About</a>
                     </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="/">Action</a>
-                            <a className="dropdown-item" href="/">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="/">Something else here</a>
-                        </div>
-                    </li>  
                 </ul>
-                
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>
     );
